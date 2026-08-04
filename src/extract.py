7569b5dea71dee471789c8d5509a6763c22d8691
zohaibtech92 <zohaibtech92@gmail.com>
2026-08-04 09:25:44 +0500

    Step 3: Built transform function to clean and structure the extract data

--- src/extract.py
+++ src/extract.py
@@ -1,12 +1,12 @@
 import requests
 
-url = 'https://api.frankfurter.dev/v1/latest?base=USD'
-
-response = requests.get(url)
-
-data = response.json()
-
-if response.status_code == 200:
-  print(data)
-else:
-  print(f"Something went wrong: {response.status_code}")
+def extract_data():
+    url = 'https://api.frankfurter.dev/v1/latest?base=USD'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Something went wrong: {response.status_code}")
+        return None
